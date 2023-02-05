f89e34a17e292f5624d9207dbfcc740d9e543f6b
Furkan <furkanakaldev@gmail.com>
2023-02-05 14:34:44 +0300

    add: handleTransfer

--- src/dai.ts
+++ src/dai.ts
@@ -3,5 +3,9 @@ import { Transfer } from "../generated/Dai/Dai"
 import { TokenTransfer } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
-
+  let transfer = new TokenTransfer(event.address.toHexString())
+  transfer.amount = event.params.wad
+  transfer.from = event.params.src
+  transfer.to = event.params.dst
+  transfer.save()
 }
